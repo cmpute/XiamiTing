7d8bce75f02faedfeb90d38f644f548c87f08a70
Jacob C <cmpute@qq.com>
2016-07-06 00:07:45 +0800

    建立分支项目，初步准备

---
+++ XiamiNetInterface/XiamiWebInterface.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace JacobC.Xiami.Net
+{
+    /// <summary>
+    /// 虾米Web端使用的Api集
+    /// </summary>
+    public class WebApi
+    {
+        /* TODO:
+         * 
+         */
+    }
+}
---
+++ XiamiNetInterface/XiamiWindowsDesktopInterface.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JacobC.Xiami.Net
+{
+    /// <summary>
+    /// 虾米Windows桌面端使用的Api集（实际上桌面程序也大量使用了Web的api）
+    /// </summary>
+    public class WindowsDesktopApi
+    {
+        /*TODO:
+         * 未登录版本
+         * av版本号可以没有，uid也可以没有 
+         * /app/xiating/collect-recommend?av=XMusic_2.0.2.1618&uid=0 推荐精选集
+         * /app/xiating/home-hot2?home_type=1&av=XMusic_2.0.2.1618&uid=0 热门专辑+热门艺人
+         * /app/xiating/home-hot2?home_type=2&av=XMusic_2.0.2.1618&uid=0 推荐专辑+推荐艺人
+         * /app/xiating/hot-music?av=XMusic_2.0.2.1618&uid=0 虾米音乐榜
+         * 
+         * 以下内容为api页面调用
+         * /pc/下存放桌面版有关的样式和js文件
+         * /api?api_key=..&api_sig=..&call_id=..&method=...&type=...&ver=...&page=.. api目录，返回json 其中如果method是Search.hotWords则是获取热词
+         * 
+         * 登录
+         * /api/oauth2/token?grant_type=password&client_id=....&username=...&password=密码加密了  ，返回json，含有token
+         * 
+         * 登录以后
+         * 
+         * 登录以后/app/的页面uid=用户uid
+         * 登录以后可以使用的api的method有Library.getLibrary, Library.getSongs（收藏的曲目）, Library.getCollects, Members.token， Search.autocomplete2, limit控制一页多少个，page可以控制页数
+         */
+    }
+}
