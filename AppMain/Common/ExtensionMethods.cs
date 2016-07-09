07f24a6173c482678bc5a3b98738e638ceb8bdf0
Jacob C <cmpute@qq.com>
2016-07-09 14:20:14 +0800

    Moved part properties of HamburgerMenu & Notification Deleted

---
+++ AppMain/Common/ExtensionMethods.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+
+namespace JacobC
+{
+    public static class ExtensionMethods
+    {
+        /// <summary>
+        /// 将<see cref="DependencyPropertyChangedEventArgs"/>转化成<see cref="ChangedEventArgs{TValue}"/>类型
+        /// </summary>
+        public static ChangedEventArgs<T> ToChangedEventArgs<T>(this DependencyPropertyChangedEventArgs e) => new ChangedEventArgs<T>((T)e.OldValue, (T)e.NewValue);
+    }
+}
