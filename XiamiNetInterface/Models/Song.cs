5e69d1fbfe2129c9562f43b293383a7f7733b008
Jacob C <cmpute@qq.com>
2016-07-09 11:46:42 +0800

    Framework Changed

---
+++ XiamiNetInterface/Models/Song.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JacobC.Xiami.Models
+{
+    public class Song
+    {
+        public int XiamiID { get; set; }
+        public string Name { get; set; }
+    }
+}
