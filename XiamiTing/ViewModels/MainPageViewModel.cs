21ef66cabf5af2f1bef0d36a4278cdb2acc26c94
Jacob C <cmpute@qq.com>
2016-07-11 21:24:54 +0800

    UI basically set

--- XiamiTing/ViewModels/MainPageViewModel.cs
+++ XiamiTing/ViewModels/MainPageViewModel.cs
@@ -45,9 +45,6 @@ namespace JacobC.Xiami.ViewModels
             await Task.CompletedTask;
         }
 
-        public void GotoDetailsPage() =>
-            NavigationService.Navigate(typeof(Views.DetailPage), Value);
-
         public void GotoSettings() =>
             NavigationService.Navigate(typeof(Views.SettingsPage), 0);
 
